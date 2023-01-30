86e1a2d74e22b60854181b1335b067ffe02c476d
April Reyes <119356418+amreyes11@users.noreply.github.com>
2023-01-29 22:03:15 -0700

    Add files via upload

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,166 @@
+--  Data is from https://github.com/vrajmohan/pgsql-sample-data/tree/master/employee
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+  dept_no VARCHAR(4) NOT NULL,
+  dept_name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (dept_no),
+  UNIQUE (dept_name)
+);
+
+CREATE TABLE employees (
+  emp_no INT NOT NULL,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  gender VARCHAR NOT NULL,
+  hire_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
+);
+
+SELECT * FROM titles
+
+--  1.  Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+--  2.  Retrieve the title, from_date, and to_date columns from the Titles table.
+SELECT title, from_date, to_date
+FROM titles;
+
+--  3.  Create a new table using the INTO clause.
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- see table; we query it w SELECT
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Join both tables on the primary key.
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+SELECT * FROM retirement_titles;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retirement_titles;
+
+--  7.  Retrieve the emp_no, first_name, and last_name columns from the Retirement table.
+SELECT emp_no, first_name, last_name, title
+FROM retirement_titles;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, title DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01')
+order by e.emp_no;
+
+SELECT * FROM unique_titles;
+
+--  17.  Retrieve the title from the unique titles table.
+SELECT title
+FROM unique_titles;
+
+-- Employee count by title
+SELECT COUNT(title)
+FROM unique_titles as ut
+LEFT JOIN dept_emp as de
+ON title = de.emp_no
+GROUP BY title
+ORDER BY title;
+
+SELECT * FROM unique_titles;
+
+--Deliverable 2
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
